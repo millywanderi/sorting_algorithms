65dd2d5616ef64730a130b2f50c296edfd2c4cb1
millywanderi <milliwanderi5@gmail.com>
2023-06-24 03:01:30 -0700

    header file

--- sort.h
+++ sort.h
@@ -41,7 +41,13 @@ void printer(int *array, const char *cont, size_t lb, size_t ub);
 void join_print(int *array, size_t lb, size_t ub, size_t mb, int *cpy);
 void pars(int *array, size_t lb, size_t ub, int *cpy);
 void heap_sort(int *array, size_t size);
+void build_down(int *array, size_t sta, size_t size, size_t end);
+void heapy(int *array, size_t size);
+void elem_swap(int *array, size_t size, int *m, int *n);
 void radix_sort(int *array, size_t size);
+void zero_init(int *array, int size);
+int max_dig(int *array, size_t size);
+void count_sort(int *array, size_t size, int loc);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
